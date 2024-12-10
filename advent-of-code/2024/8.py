d6117908053c8318fea328231d021fe6407437a1
Skenvy <17214791+Skenvy@users.noreply.github.com>
2024-12-10 17:32:04 +1100

    AOC:2024:8:2

--- advent-of-code/2024/8.py
+++ advent-of-code/2024/8.py
@@ -38,13 +38,48 @@ def parse_input_part_one(filename):
                     antinode_locations.append((pax,pay)) # add loc if within map
     return len(set(antinode_locations))
 
-# EXPLAIN PART TWO
+# Instead of only creating one antinode on either side of each dipole, each
+# dipole now creates antinodes infinitely in both directions (only bounded by
+# the size of the map), including on top of each pole in the dipole. Recount.
 
 def parse_input_part_two(filename):
+    all_symbol_locations = {}
+    map = []
     with open(filename,'r') as lines:
-        for line in lines:
-            pass # do something with each line
-    return 'TODO'
+        for line_index, line in enumerate(lines):
+            map_row = list(line)
+            if map_row[-1] == '\n':
+                del map_row[-1]
+            map.append(map_row)
+            for col_index, col_val in enumerate(map_row):
+                if col_val != '.':
+                    all_symbol_locations[col_val] = all_symbol_locations.get(col_val, [])
+                    all_symbol_locations[col_val].append((line_index, col_index))
+    # Get all antinode locations from each pair of locations for each symbol
+    antinode_locations = []
+    for symbol, symbol_locations in all_symbol_locations.items():
+        # Get each set of "pair locations" i.e. each set of 2 locations.
+        symbol_pair_locations = []
+        for k in range(len(symbol_locations)-1):
+            for j in range(k+1,len(symbol_locations)):
+                symbol_pair_locations.append((symbol_locations[k], symbol_locations[j]))
+        for spl in symbol_pair_locations:
+            # Calculate this pair's antinodes.
+            dx, dy = spl[0][0] - spl[1][0], spl[0][1] - spl[1][1]
+            possible_antinodes = [spl[0], spl[1]]
+            # Increase away from pole 0
+            k=1
+            while spl[0][0]+k*dx in range(len(map)) and spl[0][1]+k*dy in range(len(map[0])):
+                possible_antinodes.append((spl[0][0]+k*dx, spl[0][1]+k*dy))
+                k+=1
+            # decrease away from pole 1
+            k=1
+            while spl[1][0]-k*dx in range(len(map)) and spl[1][1]-k*dy in range(len(map[0])):
+                possible_antinodes.append((spl[1][0]-k*dx, spl[1][1]-k*dy))
+                k+=1
+            # Already checked that these are in-map, so just add them to global list
+            antinode_locations.extend(possible_antinodes)
+    return len(set(antinode_locations))
 
-print(f'Amount of unique antinode locations is {parse_input_part_one(f'{ADVENT_DAY}-input.txt')}')
-print(f'XYZ pt2 is {parse_input_part_two(f'{ADVENT_DAY}-input.txt')}')
+print(f'Amount of unique antinode locations (singular dipoles) is {parse_input_part_one(f'{ADVENT_DAY}-input.txt')}')
+print(f'Amount of unique antinode locations (resonant dipoles) is {parse_input_part_two(f'{ADVENT_DAY}-input.txt')}')
