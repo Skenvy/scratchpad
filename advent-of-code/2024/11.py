bdf97e2354ba7752cb17284531042512a96d5bd5
Skenvy <17214791+Skenvy@users.noreply.github.com>
2024-12-11 18:08:46 +1100

    AOC:2024:11:2

--- advent-of-code/2024/11.py
+++ advent-of-code/2024/11.py
@@ -2,7 +2,10 @@
 
 ADVENT_DAY=11
 
-# EXPLAIN PART ONE
+# A set of numbers. There are some rules for how they get updated per iteration.
+# Calculate how many numbers there are at the end of X iterations. Both problems
+# for today are doing the exact same thing, just to different X's. The solution
+# for part one is the "naive" approach of just updating the numbers in place.
 
 def parse_input_part_one(filename):
     nums = []
@@ -18,21 +21,57 @@ def parse_input_part_one(filename):
         for num in nums:
             if num == 0:
                 new_nums.append(1)
-            elif len(f'{num}') % 2 == 0:
-                new_nums.append(int(f'{num}'[:len(f'{num}')//2]))
-                new_nums.append(int(f'{num}'[len(f'{num}')//2:]))
+            elif len(str(num)) % 2 == 0:
+                new_nums.append(int(str(num)[:len(str(num))//2]))
+                new_nums.append(int(str(num)[len(str(num))//2:]))
             else:
                 new_nums.append(num*2024)
         nums = new_nums.copy()
     return len(nums)
 
-# EXPLAIN PART TWO
+# Part 2 hits a wall of being resource exhaustive following the naive approach.
+# It needs some optimisations. Although the text of the problem statement says
+# several times in bold font something about ordering or staying in line, being
+# in order is irrelevant to how the numbers evolve each iteration. There's a few
+# rules to apply to each number each iteration, but they only act on each number
+# individually independent of the surrounding numbers. So, rather than caring
+# about order, do away with order; bucket the numbers and act on the buckets!
+# Do this by acting on the "amount" of times "number X" appears, rather than
+# just on the "number X" done "amount" times.
 
 def parse_input_part_two(filename):
+    # Read in the numbers
+    nums = []
     with open(filename,'r') as lines:
         for line_index, line in enumerate(lines):
-            pass # do something with each line
-    return 'TODO'
+            digits_in_line = line.split(' ')
+            if digits_in_line[-1] == '\n':
+                del digits_in_line[-1]
+            nums.extend([int(q) for q in digits_in_line])
+    # Define the transform rules
+    def transform_number(num):
+        num_len = len(str(num))
+        if num == 0:
+            return [1]
+        elif num_len % 2 == 0:
+            return [num//10**(num_len//2), num%10**(num_len//2)]
+        else:
+            return [num*2024]
+    _CACHE_TRANSFORM = {}
+    def cache_transform_number(num):
+        if num not in _CACHE_TRANSFORM.keys():
+            _CACHE_TRANSFORM[num] = transform_number(num)
+        return _CACHE_TRANSFORM[num]
+    # Blink 75 times
+    bucketed_nums = {num:1 for num in nums}
+    for _ in range(75):
+        new_bucketed_nums = {}
+        for num, amount in bucketed_nums.items():
+            for new_num in cache_transform_number(num):
+                new_bucketed_nums[new_num] = new_bucketed_nums.get(new_num, 0)
+                new_bucketed_nums[new_num] += amount
+        bucketed_nums = new_bucketed_nums.copy()
+    return sum(bucketed_nums.values())
 
 print(f'XYZ pt1 is {parse_input_part_one(f'{ADVENT_DAY}-input.txt')}')
 print(f'XYZ pt2 is {parse_input_part_two(f'{ADVENT_DAY}-input.txt')}')
