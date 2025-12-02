2830e308ca068b06bbf0fac68ac290a7c2db5e96
Skenvy <17214791+Skenvy@users.noreply.github.com>
2025-12-02 11:24:43 +1100

    Setup AOC:2025

---
+++ advent-of-code/2025/7.py
@@ -0,0 +1,22 @@
+# https://adventofcode.com/2025/day/7
+
+ADVENT_DAY=7
+
+# EXPLAIN PART ONE
+
+def parse_input_part_one(filename):
+    with open(filename,'r') as lines:
+        for line in lines:
+            pass # do something with each line
+    return 'TODO'
+
+# EXPLAIN PART TWO
+
+def parse_input_part_two(filename):
+    with open(filename,'r') as lines:
+        for line in lines:
+            pass # do something with each line
+    return 'TODO'
+
+print(f'XYZ pt1 is {parse_input_part_one(f'{ADVENT_DAY}-input-1.txt')}')
+print(f'XYZ pt2 is {parse_input_part_two(f'{ADVENT_DAY}-input-2.txt')}')
